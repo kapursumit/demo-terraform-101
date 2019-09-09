d1301cc6c54741b0838678c770df197eab18af38
kapursumit <54902641+kapursumit@users.noreply.github.com>
2019-09-09 12:14:15 -0700

    Update main.tf
    changing the version

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
 }
 
 resource "aws_instance" "web" {
